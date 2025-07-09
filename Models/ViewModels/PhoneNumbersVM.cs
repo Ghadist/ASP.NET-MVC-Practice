1214097032da49416fd541774e16275ea35084e1
Ghadist <87462751+Ghadist@users.noreply.github.com>
2025-07-09 19:43:30 +0700

    Add files via upload

---
+++ Models/ViewModels/PhoneNumbersVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace VKR.Models.ViewModels
+{
+    public class PhoneNumbersVM
+    {
+        [Required]
+        [DisplayName("Имя")]
+        public string Name { get; set; }
+        
+        public byte addressee_ID { get; set; }
+        public byte phone_ID { get; set; }
+
+        [Required]
+        [DisplayName("Номер")]
+        public string Number { get; set; }
+        
+        [DisplayName("Заметка")]
+        public string Note { get; set; }
+
+        public List<SelectListItem> Addressees { get; set; } = new List<SelectListItem>();
+    }
+}
